8fcd5584a23b32d5b3a3fa4bada729cb47a2fcf0
Alonso Zamorano <alonso.zamorano97@gmail.com>
2020-04-24 18:02:11 -0400

    Now header has a background if the browser doesn't support backdrop-filter

--- assets/main.js
+++ assets/main.js
@@ -4,12 +4,17 @@ function setHeaderStyle() {
     let header = document.getElementsByClassName("site-header")[0];
 
     if (document.documentElement.scrollTop === 0) {
-        header.style.backgroundColor = "initial";
-        header.style.boxShadow = "initial";
+        header.style.backgroundColor = "";
+        header.style.boxShadow = "";
+        //header.style.height = "";
 
     } else {
-        console.log(window.navigator.appName);
-        header.style.backdropFilter = "blur(20px)";
         header.style.boxShadow = "0px 2px 2px rgba(0, 0, 0, 0.1)";
+        //header.style.height = "4.2rem";
+        if (CSS.supports("backdrop-filter", "blur(20px)")) {
+            header.style.backdropFilter = "blur(20px)";
+        } else {
+            header.style.backgroundColor = "white";
+        }
     }
 }
