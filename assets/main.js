636d13977f3edcbdf633eaed878456d55bf2f0f0
Alonso Zamorano <alonso.zamorano97@gmail.com>
2020-04-27 17:54:58 -0400

    Changed background color

--- assets/main.js
+++ assets/main.js
@@ -6,15 +6,9 @@ function setHeaderStyle() {
     if (document.documentElement.scrollTop === 0) {
         header.style.backgroundColor = "";
         header.style.boxShadow = "";
-        //header.style.height = "";
 
     } else {
         header.style.boxShadow = "0px 2px 2px rgba(0, 0, 0, 0.1)";
-        //header.style.height = "4.2rem";
-        if (CSS.supports("backdrop-filter", "blur(20px)")) {
-            header.style.backdropFilter = "blur(20px)";
-        } else {
-            header.style.backgroundColor = "aliceblue";
-        }
+        header.style.backgroundColor = window.getComputedStyle(document.body, null).getPropertyValue('background-color');
     }
 }
