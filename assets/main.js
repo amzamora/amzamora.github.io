e2c1c7c91d0ca834efa18bd7130c1fc7d86e1429
Alonso Zamorano <alonso.zamorano97@gmail.com>
2020-05-22 13:43:57 -0400

    Small fixes

--- assets/main.js
+++ assets/main.js
@@ -4,12 +4,13 @@ function setHeaderStyle() {
     let header = document.getElementsByClassName("site-header")[0];
     const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 
-    if (vw < 1200) {
-        if (document.documentElement.scrollTop === 0) {
-             header.style.backgroundColor = "";
-             header.style.boxShadow = "";
+    if (document.documentElement.scrollTop === 0) {
+         header.style.backgroundColor = "";
+         header.style.boxShadow = "";
 
-        } else {
+    }
+    else {
+        if (vw < 1200) {
             //header.style.backdropFilter = "blur(20px)";
             header.style.boxShadow = "0px 1px 1px rgba(0, 0, 0, 0.1)";
             header.style.backgroundColor = window.getComputedStyle(document.body, null).getPropertyValue('background-color');
